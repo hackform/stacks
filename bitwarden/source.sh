5265fb0cf2c2c4ee9eac5eb7673dde3728aa6aec
Kevin Wang <kevin@xorkevin.com>
2020-08-10 22:40:15 -0700

    Add bitwarden docker swarm config

--- bitwarden/source.sh
+++ bitwarden/source.sh
@@ -1,2 +1,6 @@
+export SERVICE_NAME=bitwarden
+export SERVICE_PORT=80
+export SERVICE_HOST=http://localhost:4242
+
 export BITWARDEN_DOMAIN=http://localhost:4242
 export BITWARDEN_PASS=$(openssl rand -base64 48)
